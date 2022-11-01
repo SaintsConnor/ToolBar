07d343ab216077c84759261b748913b38e6094bb
SaintsConnor <112587648+SaintsConnor@users.noreply.github.com>
2022-11-01 18:27:08 +0000

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,16 @@
+#ToolBar Script
+#Author's: Connor
 
+# Import libraries
+
+# Global Variables
+choice = ""
+
+# Define Functions
+
+# Main Scripts
+
+while choice != "quit":
+  print("-"*10, "Toolbar", '-'*10)
+  print("Your one stop shop for a variety of tools!")
+  print("If any issues, please contact the development team.")
