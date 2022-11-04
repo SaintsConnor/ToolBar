587c4905ddad94c1b10e2ebee8ae7bc1ca6831f2
SaintsConnor <112587648+SaintsConnor@users.noreply.github.com>
2022-11-04 13:32:08 +0000

    Update base64.py

---
+++ Modules/E_D/Encode/base64.py
@@ -0,0 +1,8 @@
+import base64
+
+message = input("Enter message to be encoded: ")
+message_bytes = message.encode('ascii')
+base64_bytes = base64.b64encode(message_bytes)
+base64_message = base64_bytes.decode('ascii')
+
+print(base64_message)
