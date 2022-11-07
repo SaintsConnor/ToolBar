e2ac2985299abe70d61a4bf5624023bb1ccfadde
SaintsConnor <112587648+SaintsConnor@users.noreply.github.com>
2022-11-07 10:53:23 +0000

    Update Encoder.py

--- Modules/E_D/Encoder.py
+++ Modules/E_D/Encoder.py
@@ -1,1 +1,29 @@
+#ToolBar Script
+#Author's: Connor
 
+# Import libraries
+import os
+import update
+
+# Global Variables
+choice = ""
+
+# Define Functions
+def Update_Script():
+  update.Update()
+
+# Main Scripts
+Update_Script()
+print("-"*10, "Toolbar", '-'*10)
+print("Your one stop shop for a variety of tools!")
+print("If any issues, please contact the development team.")
+while choice != "quit":
+  print("-"*10, "Main Menu", '-'*10)
+  print("Please type the number for one of the following algorithms or type search momentarily.")
+  print("\n")
+
+    
+  else :
+    print("Invalid Selection.")
+
+print("Thank you for using toolbar.")
