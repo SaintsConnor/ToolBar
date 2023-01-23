054275744265c7bc261d766c5bb11d69b79de353
SaintsConnor <112587648+SaintsConnor@users.noreply.github.com>
2023-01-23 16:01:06 +0000

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,8 +1,12 @@
-from distutils.core import setup
+from setuptools import setup
 
-setup(name='Toolbar',
+setup(
+      name='Toolbar',
       version='0.51',
       description='A program for red teamers',
       author='SaintsConnor',
       author_email='venomsneakymc@gmail.com',
-           )
+      packages=['ToolBar'],  #same as name
+      install_requires=['os'],
+
+)
