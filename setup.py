274636d9fb4d85b76d866647d8448808a5ac89a2
SaintsConnor <112587648+SaintsConnor@users.noreply.github.com>
2023-01-23 15:56:43 +0000

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,8 @@
+from distutils.core import setup
+
+setup(name='Toolbar',
+      version='0.51',
+      description='A program for red teamers',
+      author='SaintsConnor',
+      author_email='venomsneakymc@gmail.com',
+           )
